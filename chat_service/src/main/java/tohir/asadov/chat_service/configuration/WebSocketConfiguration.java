4dc2fdf9b4a285add64cba142a9b2342e9c97d37
Tohir <guvalakat1603@gmail.com>
2022-07-03 22:37:31 +0500

    done configuration

--- chat_service/src/main/java/tohir/asadov/chat_service/configuration/WebSocketConfiguration.java
+++ chat_service/src/main/java/tohir/asadov/chat_service/configuration/WebSocketConfiguration.java
@@ -11,11 +11,11 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 public class WebSocketConfiguration implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-
+        registry.addEndpoint("/chat").setAllowedOrigins("*").withSockJS();
     }
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
-
+        registry.setApplicationDestinationPrefixes("/app").enableSimpleBroker("/topic");
     }
 }
