6ee9638e449d9063154a4c9b2b5e992f9651f456
Tohir <guvalakat1603@gmail.com>
2022-07-03 23:05:03 +0500

    interface

--- chat_service/src/main/java/tohir/asadov/chat_service/controller/MessageController.java
+++ chat_service/src/main/java/tohir/asadov/chat_service/controller/MessageController.java
@@ -1,7 +1,25 @@
 package tohir.asadov.chat_service.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.web.bind.annotation.RestController;
+import tohir.asadov.chat_service.entity.Message;
+import tohir.asadov.chat_service.storage.UserStorage;
 
 @RestController
 public class MessageController {
+
+    @Autowired
+    private SimpMessagingTemplate simpMessagingTemplate;
+
+    @MessageMapping("/chat/{to}")
+    public void sendMessage(@DestinationVariable String to, Message message){
+        System.out.println("handling send message: " + message + " to: " + to);
+        boolean isExists = UserStorage.getInstance().getUsers().contains(to);
+        if (isExists) {
+            simpMessagingTemplate.convertAndSend("/topic/messages" + to, message);
+        }
+    }
 }
